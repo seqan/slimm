f4c90958eaf3acb1389eb4e783355a554e375afe
Temesgen H. Dadi <temehi@gmail.com>
2016-08-23 18:39:05 +0200

    [INTERNAL] removed librf library

--- librf/discrete_dist.cc
+++
@@ -1,7 +0,0 @@
-#include "librf/discrete_dist.h"
-#include <math.h>
-namespace librf {
-
-const double DiscreteDist::kLog2 = log(2);
-
-} // namespace
--- librf/weights.cc
+++
@@ -1,9 +0,0 @@
-#include "librf/weights.h"
-using namespace librf;
-
-int main(int argc, char* argv[]) {
-  weight_list w(100, 100);
-
-
-  return 0;
-}
